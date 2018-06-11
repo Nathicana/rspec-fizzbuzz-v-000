4d372297432598437755a9af7f7e0d3241d474b7
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-06-11 05:25:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(x)
-  if x % 3 == 0
-     "Fizz"
+  if x % 3 && 5 == 0
+     "FizzBuzz"
   elsif x % 5 == 0
      "Buzz"
-  elsif x % 15 == 0
-     "FizzBuzz"
+  elsif x % 3 == 0
+  "Fizz"
   else
     nil
 
